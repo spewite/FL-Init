aa17bd6c923b211247b4e25444a53c7dcfd42408
iizeta <jiz.j12.a4@gmail.com>
2024-06-23 01:14:08 +0200

    Audiyua bakarrik deskargatzea. Outputan estaduak ondo azaltzeya (Berdia, urdiña, gorriya).

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,26 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+
+const { app, BrowserWindow, ipcMain, dialog, Menu } = require('electron');
 const path = require('path');
+const fs = require('fs');
+const quote = require('shell-quote').quote;
+const { exec } = require('child_process');
+const { spawn } = require('child_process');
+
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1000, // Ancho de la ventana
-    height: 600, // Alto de la ventana
+    width: 1000,
+    height: 764,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-      // preload: path.join(__dirname, 'preload.js')
+      contextIsolation: false
     }
   });
 
   win.loadFile(path.join(__dirname, '../html/index.html'));
+
+  // Abrir DevTools automáticamente
+  win.webContents.openDevTools();
 
   const menu = Menu.buildFromTemplate([
     {
@@ -60,9 +68,9 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
-}); 
-  
+});
 
+// Manejo de IPC para abrir el diálogo de selección de archivos
 ipcMain.on('open-file-dialog', (event) => {
   dialog.showOpenDialog({
     properties: ['openDirectory']
@@ -72,5 +80,43 @@ ipcMain.on('open-file-dialog', (event) => {
     }
   }).catch(err => {
     console.error('Error al abrir el diálogo de selección de directorio:', err);
+  });
+});
+
+
+ipcMain.on('run-python-script', (event, args) => {
+  let scriptPath = path.join(__dirname, '../scripts/script_python.py');
+
+  const pythonProcess = spawn('python', [scriptPath, ...args]);
+
+  pythonProcess.stdout.on('data', (data) => {
+
+    const message = data.toString();
+    
+    if (message.toLowerCase().includes('error')) {
+      event.sender.send('python-script-error', message);
+    } else if (message.toLowerCase().includes('chunk'))  {
+      event.sender.send('python-script-info', message);
+    } else 
+    {
+      console.log(`stdout: ${message}`);
+      event.sender.send('python-script-stdout', message);
+    }
+
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    const error = data.toString();
+    console.error(`stderr: ${error}`);
+    event.sender.send('python-script-error', error);
+  });
+
+  pythonProcess.on('close', (code) => {
+    console.log(`Proceso terminado con código ${code}`);
+    if (code === 0) {
+      event.sender.send('python-script-stdout', "Script terminado con éxito");
+    } else {
+      event.sender.send('python-script-error', `Script terminado con código de error: ${code}`);
+    }
   });
 });
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  // Punto de entrada para tu aplicación
+  entry: './src/js/render.js',  // Asegúrate de que este sea el camino correcto al archivo principal de tu renderer
+
+  // Configuración de salida
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  // Configuración para módulos
+  module: {
+    rules: [
+      // Regla para archivos JavaScript
+      {
+        test: /\.js$/, // Aplica el loader a archivos .js
+        exclude: /node_modules/, // Excluye la carpeta node_modules
+        use: {
+          loader: 'babel-loader', // Utiliza babel-loader
+          options: {
+            presets: ['@babel/preset-env'] // Configura Babel para usar preset-env
+          }
+        }
+      },
+      // Regla para archivos CSS
+      {
+        test: /\.css$/, // Aplica el loader a archivos CSS
+        use: ['style-loader', 'css-loader'], // Utiliza style-loader y css-loader
+      }
+    ]
+  },
+
+  // Configuración para resolver módulos
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    fallback: {
+      "fs": false,  // No incluir fs
+      "path": false,  // No incluir path
+      "child_process": false  // No incluir child_process
+    }
+  },
+
+  // Configuración de modo
+  mode: 'development' // Cambia a 'production' cuando estés listo para desplegar
+};
